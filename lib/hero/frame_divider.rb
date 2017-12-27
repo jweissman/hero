8b3b83b92c6a1e6386803376e889fedc23237f29
Joseph Weissman <joe@bignerdranch.com>
2017-12-27 09:21:10 -0500

    remove binding from prod code!

--- lib/hero/frame_divider.rb
+++ lib/hero/frame_divider.rb
@@ -19,56 +19,50 @@ module Hero
 
     protected
     def split_with_specifications
-      begin
-        child_size_map = []
+      child_size_map = []
 
-        children.each.with_index do |child, ndx|
-          if specifies_size?(child)
-            child_size_map[ndx] = specified_size(child)
-            # elsif !partially_specifies_size?(child)
-            #   child_size_map[ndx] = unspecified_child_share
-          end
+      children.each.with_index do |child, ndx|
+        if specifies_size?(child)
+          child_size_map[ndx] = specified_size(child)
         end
-
-        total_specified_share = child_size_map.compact.inject(&:+) || 0
-        total_specified_count = child_size_map.compact.count || 0
-
-        if total_specified_count < children.count
-          default_unspecified_share = (total_size - total_specified_share) / (children.count - total_specified_count)
-          # okay, we've assigned those with direct specifications
-          # now attempt to assign those without even partial specifications...
-          children.each.with_index do |child, ndx|
-            if !specifies_size?(child) && partially_specifies_size?(child)
-              # okay, here's the rub
-              child_size_map[ndx] = [ partially_specified_size(child), default_unspecified_share ].max
-            end
-          end
-        end
-
-        final_specified_share = child_size_map.compact.inject(&:+) || 0
-        final_specified_count = child_size_map.compact.count || 0
-
-        if final_specified_count < children.count
-          # okay, now we need to distribute the REMAINING remaining over truly unspecified children
-          default_final_share = (total_size - final_specified_share) / (children.count - final_specified_count)
-          children.each.with_index do |child, ndx|
-            if !specifies_size?(child) && !partially_specifies_size?(child)
-              child_size_map[ndx] = default_final_share
-            end
-          end
-        end
-
-        children_sizes = child_size_map
-
-        # hmmmmmmmmmmmmmm
-        pts = Array.new(children.count-1) do |ndx|
-          origin + children_sizes[0..ndx].inject(&:+)
-        end
-
-        frame.slice(*pts, direction: direction)
-      rescue TypeError
-        binding.pry
       end
+
+      total_specified_share = child_size_map.compact.inject(&:+) || 0
+      total_specified_count = child_size_map.compact.count || 0
+
+      if total_specified_count < children.count
+        default_unspecified_share = (total_size - total_specified_share) / (children.count - total_specified_count)
+        # okay, we've assigned those with direct specifications
+        # now attempt to assign those without even partial specifications...
+        children.each.with_index do |child, ndx|
+          if !specifies_size?(child) && partially_specifies_size?(child)
+            # okay, here's the rub
+            child_size_map[ndx] = [ partially_specified_size(child), default_unspecified_share ].max
+          end
+        end
+      end
+
+      final_specified_share = child_size_map.compact.inject(&:+) || 0
+      final_specified_count = child_size_map.compact.count || 0
+
+      if final_specified_count < children.count
+        # okay, now we need to distribute the REMAINING remaining over truly unspecified children
+        default_final_share = (total_size - final_specified_share) / (children.count - final_specified_count)
+        children.each.with_index do |child, ndx|
+          if !specifies_size?(child) && !partially_specifies_size?(child)
+            child_size_map[ndx] = default_final_share
+          end
+        end
+      end
+
+      children_sizes = child_size_map
+
+      # hmmmmmmmmmmmmmm
+      pts = Array.new(children.count-1) do |ndx|
+        origin + children_sizes[0..ndx].inject(&:+)
+      end
+
+      frame.slice(*pts, direction: direction)
     end
 
     def child_size(child)
